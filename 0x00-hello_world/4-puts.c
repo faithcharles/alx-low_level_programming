7eb562825b0ab4566989bb787bc1cf9d4ad0bfca
faithcharles <faithcharles87@gmail.com>
2022-04-07 13:10:36 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,7 @@
  *
  *return: Always 0 (Sucess)
  */
-int main (void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	puts(""\"Programming is like building a multilingual puzzle");
+	return (0);
 }
