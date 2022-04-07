0388381bab2d23361923b7354c6ae0f5e6150c3b
faithcharles <faithcharles87@gmail.com>
2022-04-07 05:27:00 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 /**
-*
-*main -Entry point 
-*
-*return: Always 0 (sucess)
+*main -Entry point
+*my hello, printf task
+*Return: return 1 after program execution
 */
 int main (void)
-{
-printf("size of a char: %d byte(s)\n", sizeof(char));
-printf("size of an int: %d byte(s)\n", sizeos(int));
-printf(size oof a long int: %d byte(s)\n", sizeof(long int));
-printf(size of a long long int: %d byte(s)\n", sizeof(longl long int));
-printf(size of a float: %d byte(s)\n", sizeof(float));
+print("with proper grammar, but the outcome is a piece of art"\n);
 return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*
+*main -Entry point 
+*
+*return: Always 0 (sucess)
+*/
+int main (void)
+{
+printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("size of an int: %d byte(s)\n", sizeos(int));
+printf(size oof a long int: %d byte(s)\n", sizeof(long int));
+printf(size of a long long int: %d byte(s)\n", sizeof(longl long int));
+printf(size of a float: %d byte(s)\n", sizeof(float));
+return (0);
+}
